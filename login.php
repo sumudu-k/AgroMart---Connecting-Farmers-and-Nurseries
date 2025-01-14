c7d35e5b85beeb5bf76718c8d85abc9fac2675d2
sumudu-k <lakshankulathunga21@gmail.com>
2025-01-14 09:09:30 +0530

    Add login page with form for user authentication

---
+++ login.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <title>Login</title>
+</head>
+
+<body>
+    <h2>Login</h2>
+    <form action="login.php" method="post">
+        <input type="email" name="email" placeholder="Email" required><br>
+        <input type="password" name="password" placeholder="Password" required><br>
+        <button type="submit" name="login">Login</button>
+    </form>
+    <p><a href="forgotpw.php">Forgot Password?</a></p>
+</body>
+
+</html>
