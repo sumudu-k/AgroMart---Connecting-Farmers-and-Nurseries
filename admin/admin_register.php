5041836e2b9e34ffa809a7194d26b4967d833e60
rashmika135 <ashan1rashmika2@gmail.com>
2025-01-29 12:45:01 +0530

    completed the admin register page by adding html

--- admin/admin_register.php
+++ admin/admin_register.php
@@ -33,3 +33,11 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     }
 }
 ?>
+
+<form method="POST" action="admin_register.php">
+    <input type="text" name="username" placeholder="Admin Username" required><br>
+    <input type="email" name="email" placeholder="Email" required><br>
+    <input type="email" name="confirm_email" placeholder="Confirm Email" required><br>
+    <input type="password" name="password" placeholder="Password" required><br>
+    <button type="submit">Register</button>
+</form>
