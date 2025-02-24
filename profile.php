98e94993f517463675b8b32c4ba433eca9f8828b
sumudu-k <lakshankulathunga21@gmail.com>
2025-02-24 20:05:03 +0530

    save variable values geting from session variables

--- profile.php
+++ profile.php
@@ -1,7 +1,7 @@
 <?php
 session_start();
 include 'config.php';
-
+include 'navbar.php';
 
 if (!isset($_SESSION['user_id'])) {
     header('Location: login.php');
@@ -16,7 +16,16 @@ $stmt->bind_param('i', $user_id);
 $stmt->execute();
 $result = $stmt->get_result();
 $userData = $result->fetch_assoc();
-print_r($userData);
+
+if (isset($_POST['update'])) {
+    $username = $_POST['username'];
+    $email = $_POST['email'];
+    $password = $_POST['password'];
+    $contact_number = $_POST['contact_number'];
+    $address = $_POST['address'];
+}
+
+
 ?>
 
 <!DOCTYPE html>
@@ -28,48 +37,63 @@ print_r($userData);
     <title>Profile</title>
 </head>
 
+<h1>Edit Profile</h1>
+
+<script>
+function updateProfile() {
+
+}
+</script>
+
+
 <body>
-    <form>
+    <form action="profile.php?user_id=" method="post">
         <table>
-            <thead>
-                <th>A</th>
-                <th>B</th>
-            </thead>
             <tbody>
                 <tr>
                     <td><label>Username</label></td>
-                    <td><input type='text' value=<?= $userData['username'] ?>></td>
+                    <td><input type='text' name="username" value=<?= $userData['username'] ?>></td>
                 </tr>
 
                 <tr>
                     <td><label>Email</label></td>
-                    <td><input type='text' value=<?= $userData['email'] ?>></td>
+                    <td><input type='text' name="email" value=<?= $userData['email'] ?>></td>
                 </tr>
 
                 <tr>
                     <td><label>New Password</label></td>
-                    <td><input type='text'></td>
+                    <td><input type='text' name="password"></td>
                 </tr>
 
                 <tr>
                     <td><label>Confirm Password</label></td>
-                    <td><input type='text'></td>
+                    <td><input type='text' name="confirm_password"></td>
                 </tr>
 
 
                 <tr>
                     <td><label>Contact Number</label></td>
-                    <td><input type='number' value=<?= $userData['contact_number'] ?>></td>
+                    <td><input type='number' name="contact_number" value=<?= $userData['contact_number'] ?>></td>
                 </tr>
 
                 <tr>
                     <td><label>Address</label></td>
-                    <td><input type='text' value=<?= $userData['address'] ?>></td>
+                    <td><input type='text' name="address" value=<?= $userData['address'] ?>></td>
+                </tr>
+
+                <tr>
+                    <td></td>
+                    <td><button name="update" onClick="updateProfile">Update</button></td>
                 </tr>
 
             </tbody>
         </table>
     </form>
+
+    <label>Delete my profile</label><br>
+    <span> This action can not be undo. Please make sure are you want to delete your account. All posted advertisemets
+        will remove associated with you </span><br>
+    <button> Delete Account</button>
 </body>
 
 </html>
