1b370227b927d2eb4a976563f7b8bcb0040a30c2
rashmika135 <ashan1rashmika2@gmail.com>
2025-03-02 20:23:58 +0530

    add post request form for user submissions

---
+++ post_request.php
@@ -0,0 +1,16 @@
+<form method="post">
+    <label>Subject:</label>
+    <input type="text" name="subject" required><br>
+
+    <label>Description:</label>
+    <textarea name="description" required></textarea><br>
+
+    <label>Contact Number:</label>
+    <input type="text" name="contact" required><br>
+
+    <label>District:</label>
+    <select name="district">
+    </select><br>
+
+    <button type="submit">Submit Request</button>
+</form>
