64677b9873fc047e6a31efc7a40503abef15ee27
tharindu <tharindusubodha0@gmail.com>
2025-03-04 21:02:29 +0530

    added conflicted files

--- home.php
+++ home.php
@@ -3,7 +3,6 @@ session_start();
 include 'config.php';
 include 'navbar.php';
 
-// Fetch categories from the database
 $query = "SELECT * FROM categories";
 $result = $conn->query($query);
 
@@ -35,7 +34,7 @@ $ads_result = $conn->query($ads_query);
         margin: 0;
     }
 
-    /* Banner Image */
+    /* banner Image */
     .banner-image {
         position: relative;
         width: 100%;
@@ -66,7 +65,7 @@ $ads_result = $conn->query($ads_query);
     }
 
 
-    /* Category Section */
+    /*category Section */
     /* all categories Container */
     .category-container {
         display: flex;
@@ -88,7 +87,7 @@ $ads_result = $conn->query($ads_query);
         margin-bottom: 20px;
     }
 
-    /* For category card */
+    /*  category card */
     .category-card {
         display: flex;
         flex-direction: column;
@@ -127,7 +126,7 @@ $ads_result = $conn->query($ads_query);
     }
 
 
-    /* Welcome Section */
+    /* welcome Section */
     .welcome-section {
         display: flex;
         align-items: center;
@@ -437,7 +436,7 @@ $ads_result = $conn->query($ads_query);
 
 
 
-    /* For Mobile devices */
+    /*  Mobile devices */
     @media screen and (max-width: 480px) {
         .main-container {
             width: 95%;
@@ -620,7 +619,7 @@ $ads_result = $conn->query($ads_query);
 
 <body>
 
-    <!-- Home page banner slider -->
+    <!-- home page banner slider -->
     <div class="banner-image">
         <img class="banner-slides active" src="images/cover.png" alt="Slide 1">
         <img class="banner-slides"
@@ -646,7 +645,7 @@ $ads_result = $conn->query($ads_query);
         setTimeout(showSlides, 3000);
     }
 
-    showSlides(); // Start the slideshow
+    showSlides();
     </script>
 
     <div class="main-container">
@@ -669,7 +668,7 @@ $ads_result = $conn->query($ads_query);
             <?php endwhile; ?>
         </div>
 
-        <!-- Welcome section -->
+        <!-- welcome section -->
         <section class="welcome-section">
             <div class="welcome-image">
                 <img src="images/inner_home_05-1024x768.jpg" alt="Gardening Image">
@@ -689,7 +688,6 @@ $ads_result = $conn->query($ads_query);
             </div>
         </section>
 
-        <!-- Ads area HTML -->
         <div class="ads-container">
             <h1 class="ads-title">Find What You Want Here</h1>
             <div class="sugestion-ads">
@@ -719,7 +717,6 @@ $ads_result = $conn->query($ads_query);
         <section class="contact-section">
             <h1>Contact</h1>
             <div class="contact-container">
-                <!-- Contact Info -->
                 <div class="contact-info">
                     <h2 class="contact-title">Contact Information</h2>
 
